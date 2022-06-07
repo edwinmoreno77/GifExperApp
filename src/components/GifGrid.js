18567975678b49e235da8bdf78d8b6fb09fab804
Edwin Moreno <edwinmoreno77@gmail.com>
2022-06-07 09:26:50 -0400

    se actualizo con useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,38 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import { GifGridItem } from './GifGridItem';
+import { getGifs } from '../Helpers/getGifs.js';
 
 export const GifGrid = ({ category }) => {
 
     const [images, setImages] = useState([]);
 
     useEffect(() => {
-        getGifs();
-    }, [])
-
-    const getGifs = async () => {
-        const url = 'https://api.giphy.com/v1/gifs/search?limit=10&q=rick+and+morty&api_key=r4dyUY0kSk3BPz4M2MYdZcnP98WbS6qg';
-
-        const resp = await fetch(url);
-        const { data } = await resp.json();
-
-        const gifs = data.map(img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
-
-
-
-        setImages(gifs);
-    }
-
+        getGifs(category)
+            .then(imgs => setImages(imgs));
+    }, [category])
 
     return (
-        <div>
+        <>
             <h3>{category}</h3>
-            <ol>
+            <div className="card-grid">
                 {
                     images.map(img => (
                         <GifGridItem
@@ -41,7 +23,7 @@ export const GifGrid = ({ category }) => {
                         />
                     ))
                 }
-            </ol>
-        </div>
+            </div>
+        </>
     )
 }
